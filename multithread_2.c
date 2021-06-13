fae3895f44afd686a3c96209012ee7bc73845624
vouidaskis <mpampis.vouidaskis@gmail.com>
2021-06-13 18:34:26 +0300

    last

--- multithread_2.c
+++ multithread_2.c
@@ -1,26 +1,26 @@
 //6007 βουιδασκης χαραλαμπος
 #include <stdio.h>
 #include "util.h"
-#include <pthread.h>//sumperilambanoume th bibliothhkh gia pthread
-pthread_mutex_t mutex,mute;//dhmiourgia enos mutex gia sugxronismo kai ara apofugh race conditions kai sugkekrimena gia amoibaio apokleismo
+#include <pthread.h>
+pthread_mutex_t mutex,mute;//create a mutex for synchronization and avoid  race conditions 
 
 int rep,argc,k;
 
 int krata();
 
-void *func(void *param) {//sunarthsh gia to nhmaτα
+void *func(void *param) {//function for threads
 int j;
 	char *p = (char *) param;
-	pthread_mutex_lock (&mutex); //mpainei athe argument diaforetika mono toy logo toy lock
+	pthread_mutex_lock (&mutex); 
 	init();
-	pthread_mutex_unlock (&mutex);//hekleidwnei kai mpainei to epomeno
-	krata();//exoyme thn synarthsh poy miwnei kathe fora miwn ena 
-	pthread_mutex_lock (&mute);//exoyme to lock poy den to afhnei na perasei mexrh na vrei unlock
-	 krata();//gia na energopoihsei to unlock
-	 pthread_mutex_lock (&mute);//perimenei to unlock
-	 pthread_mutex_unlock (&mute);//kanei unlock gia thn synexeia pou den sumrtasxei h synarthsh krata
+	pthread_mutex_unlock (&mutex);//unlock and Continue next
+	krata(); 
+	pthread_mutex_lock (&mute);
+	krata();//for activate unlock
+	pthread_mutex_lock (&mute);//wait unlock
+	pthread_mutex_unlock (&mute);// unlock for Continuity where not join the function
 for(j=0; j<rep; j++){
-	pthread_mutex_lock (&mutex);//kanei oti kai prin
+	pthread_mutex_lock (&mutex);
 	display(p);
 	pthread_mutex_unlock (&mutex);
 }
@@ -36,25 +36,25 @@ rep=atoi(argsv[1]);
 
 
 	pthread_mutexattr_t attr;
-	pthread_mutexattr_init(&attr); //arxikopoihsh antikeimenwn idiothtwn mutex
-	pthread_mutex_init(&mutex, &attr);////arxhkopoiei to kleidh me pinakaarxikopoihsh tou mutex
+	pthread_mutexattr_init(&attr); 
+	pthread_mutex_init(&mutex, &attr);
 	pthread_t id[argc];
  	pthread_mutex_init(&mutex, &attr);
 
-	for(i=2; i<argc; i++){//dhmiourgia pthread gia to prwto nhma, dhladh gia ths sunarthsh
+	for(i=2; i<argc; i++){//create pthread
 	pthread_create(&id[i],NULL , func,(void *) argsv[i]);
 
 
 }
-	for(i=2; i<argc; i++){//perimenei na teleiwsoun ta nimata
+	for(i=2; i<argc; i++){//wait until finish the thread
 
 	pthread_join(id[i], NULL);
 }	pthread_mutex_destroy(&mutex);
 }
 
 
-int krata(){//kathe fora pou mpainei shmainei pws ektelesthike mia fora to init an kanri tis aferseis oses fores mpain kia paei <=1 tote 
-//snexizrei gia thn display
+int krata(){
+
 k--;
 if(k<=1){
 	 pthread_mutex_unlock (&mute);
